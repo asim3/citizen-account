2ecdca0e57589b6429d5837358d19e830a0cd947
asim3 <asimwebapps@gmail.com>
2020-01-23 00:16:34 +0300

    .\website\

--- aws-cp.cmd
+++ aws-cp.cmd
@@ -1,1 +1,1 @@
-aws s3 cp c:\www\citizen-account\website\ s3://web-citizen-account/website/ --acl public-read --recursive
+aws s3 cp .\website\ s3://web-citizen-account/website/ --acl public-read --recursive
