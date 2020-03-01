753460d66c41630a6d3a74897ca61b2a666b41e7
asim3 <asimwebapp@gmail.com>
2020-03-01 17:20:29 +0300

    updating index dropdown

--- v5/static/js/main.js
+++ v5/static/js/main.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-  $(".dropdown-submenu a.nav-link").on("click", function(e) {
+$(document).ready(function () {
+  $(".dropdown-submenu a.nav-link").on("click", function (e) {
     $(this)
       .next("ul")
       .toggle();
     e.stopPropagation();
     e.preventDefault();
+  });
+
+  $(".dropdown, .btn-group").hover(function () {
+    var dropdownMenu = $(this).children(".dropdown-menu");
+    if (dropdownMenu.is(":visible")) {
+      dropdownMenu.parent().toggleClass("open");
+    }
   });
 });
