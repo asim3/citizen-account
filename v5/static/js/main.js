1e37153c8a914393ec00b45f05021124333f6235
asim3 <asimwebapps@gmail.com>
2020-03-08 16:06:42 +0300

    fixing homepage according to customer

--- v5/static/js/main.js
+++ v5/static/js/main.js
@@ -1,5 +1,9 @@
-$(document).ready(function () {
-  $(".dropdown-submenu a.nav-link").on("click", function (e) {
+function show_calculator() {
+  console.log("calculator here");
+}
+
+$(document).ready(function() {
+  $(".dropdown-submenu a.nav-link").on("click", function(e) {
     $(this)
       .next("ul")
       .toggle();
@@ -7,14 +11,14 @@ $(document).ready(function () {
     e.preventDefault();
   });
 
-  $("nav .dropdown, .btn-group").hover(function () {
+  $("nav .dropdown, .btn-group").hover(function() {
     var dropdownMenu = $(this).children(".dropdown-menu");
     if (dropdownMenu.is(":visible")) {
       dropdownMenu.parent().toggleClass("open");
     }
   });
 
-  $("#show-calculator-video h4").click(function () {
+  $("#show-calculator-video h4").click(function() {
     $("#show-calculator-video iframe").show();
-  })
+  });
 });
