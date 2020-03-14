be70896e1cbf18ea08a45c1f8426961fffc202b2
asim3 <asimwebapps@gmail.com>
2020-03-15 00:28:36 +0300

    adding v7

---
+++ v7/static/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $(".dropdown-submenu a.nav-link").on("click", function(e) {
+    $(this)
+      .next("ul")
+      .toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+
+  $("nav .dropdown, .btn-group").hover(function() {
+    var dropdownMenu = $(this).children(".dropdown-menu");
+    if (dropdownMenu.is(":visible")) {
+      dropdownMenu.parent().toggleClass("open");
+    }
+  });
+
+  $("#show-calculator-video h4").click(function() {
+    $("#show-calculator-video iframe").show();
+  });
+
+  $(".custom-file-input").on("change", function() {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+  });
+
+  if($.fn.hasOwnProperty('hijriDatePicker')) {
+    $("#hijri-date-input").hijriDatePicker({
+      hijri:true,
+      showTodayButton: true
+    });
+  }
+});
